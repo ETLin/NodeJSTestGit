6c3f1bca0c6ad881975c139870cc45ab181ed130
ETLin <chrisfollen@gmail.com>
2014-04-20 16:12:51 +0800

    Request

---
+++ HTTPFirstTest.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var host = '127.0.0.1';
+
+http.createServer(function (req, res) {
+
+console.log(req.method + ' ' + req.url + ' HTTP/' + req.httpVersion);
+
+console.log('Host:' + host);
+}).listen(5000, host);
